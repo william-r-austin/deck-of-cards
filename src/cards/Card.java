fc5a3c8afabafe64bbf7c0d28821df83adfcaf81
William Austin <william.r.austin@gmail.com>
2014-12-22 08:02:09 -0500

    Create Card.java

---
+++ src/cards/Card.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cards;
+
+/**
+ * Interface for functionality common to cards. For now, we are only dealing 
+ * with poker cards.
+ */
+public interface Card {
+
+}
